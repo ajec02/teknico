bb09658e1c7cd811737006fc845b2f679f623b00
ajec02 <ajec02@gmail.com>
2026-08-06 13:34:06 +0100

    Ajuste final em testes e documentação do Teknico OS

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,20 +1,20 @@
-// Testes de Interface para o Sistema Suporte
+// Testes de Interface para o Sistema Teknico
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:suporte/main.dart';
-import 'package:suporte/providers/app_state.dart';
+import '../lib/main.dart';
+import '../lib/providers/app_state.dart';
 
 void main() {
-  testWidgets('Carregamento do Sistema Suporte', (WidgetTester tester) async {
+  testWidgets('Carregamento do Sistema Teknico', (WidgetTester tester) async {
     await tester.pumpWidget(
       ChangeNotifierProvider(
         create: (_) => AppState(),
-        child: const SuporteApp(),
+        child: const TeknicoApp(),
       ),
     );
 
     // Verificar se o ecrã de conexão é carregado com o título
-    expect(find.text('SUPORTE OS'), findsOneWidget);
+    expect(find.text('TEKNICO OS'), findsOneWidget);
   });
 }
